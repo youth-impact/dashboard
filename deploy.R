e7b1b202fbf199c2bc1c4f5b74d436b19bfb7b03
Jake Hughey <jakejhughey@gmail.com>
2023-02-07 18:40:20 -0600

    persist.

--- deploy.R
+++ deploy.R
@@ -2,12 +2,13 @@
 # done: added the corresponding email address as viewer on the drive folder
 
 if (Sys.getenv('GITHUB_ACTIONS') == 'true') {
-  # if (!dir.exists('secrets')) dir.create('secrets')
-  path = 'google_token.json'
   # on GitHub Actions, Sys.getenv('GOOGLE_TOKEN') should be a json string
-  cat(Sys.getenv('GOOGLE_TOKEN'), file = path) # write the json string to a file
-  # the file should be sent to shinyapps.io, but something's not working
-  cat(glue::glue('GOOGLE_TOKEN={path}\n'), file = '.Renviron', append = TRUE)
+  # so we write the multi-line json string to a file
+  # and write the path to the file (wrt R/) to .Renviron
+  # now the json file and .Renviron should be sent to shinyapps.io
+  filename = 'google_token.json'
+  cat(Sys.getenv('GOOGLE_TOKEN'), file = file.path('R', filename))
+  cat(glue::glue('GOOGLE_TOKEN={filename}\n'), file = '.Renviron', append = TRUE)
 }
 
 shiny_cred = if (Sys.getenv('SHINY_APPS_IO') == '') {
